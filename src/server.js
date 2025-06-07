47a9548f482731e8c32869438544b62292df0177
Roma <roma.ricchiardi@gmail.com>
2025-06-07 01:29:28 -0300

    mejoras de inicio

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app/app.js'; // Importa la configuración de Express
-import { sequelize } from './app/models/index.js'; // Importa Sequelize (asegúrate de tener export en index.js)
+import { sequelize } from './app/models/index.js'; // Importa Sequelize 
 
 const PORT = process.env.PORT || 3000;
 
