b665d9dd8101fd89887d98a9213eed108a848ad7
Roma <roma.ricchiardi@gmail.com>
2025-06-17 04:06:36 -0300

    TABLAS

--- src/public/js/admisiones.js
+++ src/public/js/admisiones.js
@@ -5,9 +5,12 @@ $(document).ready(function () {
 		const dt = tabla.DataTable({
 			language: { url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json' },
 			paging: true,
-			pageLength: 5,
-			searching: true,
-			ordering: true,
+      		pageLength: 10,
+      		searching: true,
+      		ordering: true,  
+		 	destroy: true,
+      		responsive: true,
+		  	scrollX: false,
 			columnDefs: [{ targets: [8], orderable: false, searchable: false }],
 		});
 
--- src/public/js/habitacion.js
+++ src/public/js/habitacion.js
@@ -16,13 +16,13 @@ $(document).ready(function () {
     language: {
       url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
     },
-    paging: true,
-    pageLength: 5,
-    searching: true,
-    ordering: true,
-    destroy: true,
-    responsive: true,
-    scrollX: false,
+    	paging: true,
+      pageLength: 10,
+      searching: true,
+      ordering: true,  
+		  destroy: true,
+      responsive: true,
+		  scrollX: false,
     columnDefs: [{ targets: 3, orderable: false, searchable: false }],
   });
 
