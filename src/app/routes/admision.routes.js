870b3e4af4a868ec2d247613fbb1857e8942db4d
Roma <roma.ricchiardi@gmail.com>
2025-06-16 21:35:17 -0300

    correccion de ruta

--- src/app/routes/admision.routes.js
+++ src/app/routes/admision.routes.js
@@ -7,19 +7,21 @@ import {
 	deleteAdmision,
 	getOpcionesAdmision,
 	validarAdmisionPorDNI,
-	buscarAdmisionVigente,
-	darAltaPaciente,
 } from '../controllers/admision.controller.js';
+import { 
+	verificarAdmisionActiva,
+    buscarAdmisionVigente,
+	darAltaPaciente, 
+} from '../controllers/alta.controller.js';
 
 const router = Router();
 
-
 // Rutas específicas primero
-router.get('/validar-dni/:dni', validarAdmisionPorDNI); 
+router.get('/validar-dni/:dni', validarAdmisionPorDNI);
+router.get('/verificar/:id_paciente', verificarAdmisionActiva);
 router.get('/paciente/:dni/admisiones-vigentes', buscarAdmisionVigente);
 router.post('/paciente/:dni/alta', darAltaPaciente);
 router.get('/opciones', getOpcionesAdmision);
-
 // Rutas CRUD generales
 router.get('/', getAdmisiones);
 router.get('/:id', getAdmisionById);
