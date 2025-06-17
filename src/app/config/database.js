eb88a62a04619bdc4fdde64967f9766cbc9fa351
Roma <roma.ricchiardi@gmail.com>
2025-06-17 20:52:42 -0300

    Revert "base"
    This reverts commit 45af8e24dda13482a8057c31bdc063c4eab26e15.

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,18 +1,15 @@
 export default {
 	username: process.env.DB_USER || 'root',
-	password: process.env.DB_PASSWORD || 'CWrDqFHaxvJpmGdajfWBRwnQgLuvDFpL',
+	password: process.env.DB_PASSWORD || '',
 	database: process.env.DB_NAME || 'hospital_db',
-	host: process.env.DB_HOST || 'hopper.proxy.rlwy.net',
-	port: process.env.DB_PORT || 32611,
+	host: process.env.DB_HOST || '127.0.0.1',
+	port: process.env.DB_PORT || 3306,
 	dialect: 'mysql',
 	logging: false,
-	timezone: '-03:00', // 📍 Ajuste a hora de Argentina
 	dialectOptions: {
 		ssl: {
 			require: true,
-			rejectUnauthorized: false,
-		},
-		dateStrings: true,
-		typeCast: true,
-	},
+			rejectUnauthorized: false
+		}
+	}
 };
