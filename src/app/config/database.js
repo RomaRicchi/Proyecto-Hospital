34bbd93fb5343ff925b395045591bee76ce8d99f
Roma <roma.ricchiardi@gmail.com>
2025-06-17 20:58:45 -0300

    base

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,15 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
-	username: process.env.DB_USER || 'root',
-	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_NAME || 'hospital_db',
-	host: process.env.DB_HOST || '127.0.0.1',
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	host: process.env.DB_HOST,
 	port: process.env.DB_PORT || 3306,
 	dialect: 'mysql',
 	logging: false,
+	timezone: '-03:00',
 	dialectOptions: {
 		ssl: {
 			require: true,
-			rejectUnauthorized: false
-		}
-	}
+			rejectUnauthorized: false,
+		},
+		dateStrings: true,
+		typeCast: true,
+	},
 };
