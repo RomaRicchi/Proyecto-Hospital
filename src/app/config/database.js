db8f2cfa135c7bebcf9d4e379b929a749364e53b
Roma <roma.ricchiardi@gmail.com>
2025-06-17 21:04:16 -0300

    base

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,15 +1,12 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 export default {
-	username: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT || 3306,
+	username: 'root',
+	password: 'CWrDqFHaxvJpmGdajfWBRwnQgLuvDFpL',
+	database: 'railway',
+	host: 'mysql-z9hz.railway.internal',
+	port: 3306,
 	dialect: 'mysql',
 	logging: false,
-	timezone: '-03:00',
+	timezone: '-03:00', // Hora de Argentina
 	dialectOptions: {
 		ssl: {
 			require: true,
